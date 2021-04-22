1c3f39cdf2eb0d00be1d39ed11cc1b06eb35d8ad
vladho <vladhopilo@gmail.com>
2021-04-22 16:47:25 +0300

    done

--- src/redux/contacts/contactsConstants.js
+++
@@ -1,6 +0,0 @@
-const ADDCONTACT = "$item/addContact";
-const DELETECONTACT = "@item/deleteContact";
-const FILTERCONTACTS = "@filter/filterContacts";
-const GETCONTACTS = "@getContacts";
-
-export { ADDCONTACT, DELETECONTACT, FILTERCONTACTS, GETCONTACTS };
